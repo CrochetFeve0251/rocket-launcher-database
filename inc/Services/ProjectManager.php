1dd9a05ab3509a13d7b5eb95c1c8fd3fe0a947be
cyrille <coquardcyr@gmail.com>
2023-04-10 11:51:39 +0200

    Added changed ProjectManager.php to adapt to the new logic

--- inc/Services/ProjectManager.php
+++ inc/Services/ProjectManager.php
@@ -89,7 +89,6 @@ class ProjectManager
     {
         $content = $this->filesystem->read(self::COMPOSER_FILE);
         $json = json_decode($content,true);
-
         return $json && key_exists('extra', $json) && key_exists('mozart', $json['extra']) && key_exists('packages', $json['extra']['mozart']) && in_array('wp-launchpad/autoresolver', $json['extra']['mozart']['packages']);
     }
 }
