7a0c13591c0c9b1aa532e327cf04efa73311590e
Cyrille C <18537428+CrochetFeve0251@users.noreply.github.com>
2023-05-16 22:57:41 +0200

    Added base for field generation

---
+++ inc/Entities/FileType.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace LaunchpadBerlinDB\Entities;
+
+class FileType
+{
+
+}
---
+++ inc/Services/FieldManager.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace LaunchpadBerlinDB\Services;
+
+use LaunchpadBerlinDB\Entities\Field;
+use LaunchpadBerlinDB\Entities\FileType;
+
+class FieldManager
+{
+    protected $fields = [];
+
+    public function add(Field $field) {
+
+    }
+
+    public function render(FileType $type): string {
+
+    }
+}
